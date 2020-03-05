2288f88f80a6448aadff376595422baefc9e0b65
Nikola Marcic <marcic74@gmail.com>
2020-03-05 15:41:58 +0100

    Brisanej AUTH

--- src/Auth/routes_protected.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$router->post('/auth/logout', 'JwtLoginController@logout');
-$router->post('/auth/refresh', 'JwtLoginController@refresh');
-$router->get('/me', 'JwtLoginController@me');
--- src/Auth/routes_public.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$router->post('/auth/login', 'JwtLoginController@login');
-$router->post('/auth/register', 'JwtLoginController@register');
-$router->post('/auth/password/email', 'ForgotPasswordController@sendResetLinkEmail');
-$router->post('/auth/password/reset', 'ResetPasswordController@reset');
